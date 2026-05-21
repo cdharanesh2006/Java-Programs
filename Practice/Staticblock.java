5e235701f710aea4ab6b65c2bd33e900d9e86e74
Dharanesh <cdharanesh2006@gmail.com>
2026-05-21 14:48:13 +0530

    Add files via upload

---
+++ Practice/Staticblock.java
@@ -0,0 +1,9 @@
+public class Staticblock{
+    static{
+        System.out.println("This Is Dharanesh");
+    }
+        public static void main(String[]args){
+            System.out.println("Shanmugha College");
+        
+    }
+}
