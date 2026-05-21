eb47d9dedbfe9a915316c520ec6c7e3a245344b0
Dharanesh <cdharanesh2006@gmail.com>
2026-05-21 14:25:22 +0530

    Add files via upload

---
+++ Practice/Staticmethod.java
@@ -0,0 +1,11 @@
+
+class Student{
+    static void College(){
+        System.out.println("Shamugha College");
+    }
+}
+public class Staticmethod {
+    public static void main(String[] args) {
+        Student.College();
+    }
+}
